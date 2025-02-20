29052df22de6cf040c85d2789a5f6e6d536ababc
Ivan Chapanov <ichapanov@navicons.com>
2025-02-21 03:12:13 +0500

    edited personal calc added studio_info buttons

--- Maria_Design_bot/main.py
+++ Maria_Design_bot/main.py
@@ -2,10 +2,11 @@ from curses.ascii import isdigit
 import telebot
 from telebot import types
 import os
+import textwrap
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
-
+property_type = None
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -43,36 +44,69 @@ def service_description(message):
 			bot.send_photo(message.chat.id,photo)
 
 def studio_info(message):
-	bot.send_message(message.chat.id, f'Уважаемый {message.from_user.first_name}!\n'
-									  f'здесь представлена информация\n'
-									  f'о студии'
-					 )
+	markup = types.InlineKeyboardMarkup()
+	pictures = types.InlineKeyboardButton(textwrap.fill(f'Интерьерные\n '
+														f'картины'), callback_data='Интерьерные картины')
+	about = types.InlineKeyboardButton('Обо мне', callback_data='Обо мне')
+	contacts = types.InlineKeyboardButton('Контакты', callback_data='Контакты')
+	markup.add(about, contacts,pictures)
+	bot.send_message(message.chat.id, f'Познакомимся по-ближе',
+					 reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call:True)
+def info(call):
+	if call.data == 'Интерьерные картины':
+		bot.send_message(call.from_user.id, f'Здесь будет инфо про картины')
+	elif call.data == 'Обо мне':
+		bot.send_message(call.from_user.id, f'Здесь будет информация Обо мне')
+	elif call.data == 'Контакты':
+		bot.send_message(call.from_user.id, f'Здесь будет контактная информация')
+
 
 def personal_calc(message):
 	# bot.send_message(message.chat.id, 'Пожалуйста, выберите услуги')
 	markup = types.InlineKeyboardMarkup()
-	full_project = types.InlineKeyboardButton('Под ключ',callback_data='UnderKey')
-	project = types.InlineKeyboardButton('Дизайн-проект', callback_data='Design_project')
-	express = types.InlineKeyboardButton('Экспресс', callback_data='express_project')
+	full_project = types.InlineKeyboardButton('Под ключ',callback_data='Дизайн-проект под ключ')
+	project = types.InlineKeyboardButton('Дизайн-проект', callback_data='Дизайн-проект')
+	express = types.InlineKeyboardButton('Экспресс', callback_data='Экспресс проект')
 	project_about = types.InlineKeyboardButton('Подробнее чем отличаются услуги', callback_data='about_services')
 	markup.add(full_project, project,express, project_about)
 	bot.send_message(message.chat.id, f'Пожалуйста, выберите тип услуги',
 					 reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_property_type(call):
+	global property_type
+	property_type = call.data
+	if property_type == 'about_services':
+		service_description(call.message)
+		personal_calc(call.message)
+	else:
+		bot.send_message(call.from_user.id, f'Вы выбрали: {property_type}')
+		# bot.register_next_step_handler(call.message, square_input)
+		square_input(call.message)
+
+def square_input(message):
 	bot.send_message(message.chat.id,'Введите площадь помещения:')
 	bot.register_next_step_handler(message, write_square)
 
 def write_square(message):
 	while True:
 		square = message.text
+		price = float()
+		if property_type == 'Дизайн-проект под ключ':
+			price = 2500
+		elif (property_type == 'Дизайн-проект') or (property_type == 'express_project' and float(square) <=5):
+			price = 2000
+		elif property_type == 'Экспресс проект' and float(square) >5:
+			price = 1800
 		try:
-			calc = float(square)*2500
+			calc = float(square) * price
 			period = int(0)
 			if 0 <= float(square) <= 15:
-				period = 10
-			elif 16 <= float(square) <= 50:
-				period = 20
-			elif float(square) > 50:
-				period = float(square) * 0.5
+				period = 14
+			elif float(square) > 15:
+				period = float(square) * 1
 			bot.send_message(message.chat.id, f'{str(calc)} рублей\n'
 							 					   f'{int(period)} рабочих дней на выполнение проекта')
 			break
