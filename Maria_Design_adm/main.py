e9d084b37c985ab4879fa090b67c9f7b66914bcb
Ivan Chapanov <ichapanov@navicons.com>
2025-03-25 01:55:54 +0500

    added sending contacts of users call requests

--- Maria_Design_adm/main.py
+++ Maria_Design_adm/main.py
@@ -5,9 +5,12 @@ import datetime
 from config import TOKEN
 import json
 import psycopg2
+import schedule
+import time
+from threading import Thread
 
 bot = telebot.TeleBot(TOKEN)
-
+CHAT_ID = '178945372'
 
 user_data = {}
 
@@ -71,6 +74,63 @@ def fetch_all_data_from_db():
 	column_names = [i[0] for i in cursor.description]
 	return column_names, rows
 
+def fetch_new_contact():
+	conn = psycopg2.connect(
+		dbname=db_config['dbname'],
+		user=db_config['user'],
+		password=db_config['password'],
+		host=db_config['host'],
+		port=db_config['port']
+	)
+	cursor = conn.cursor()
+	cursor.execute('SELECT first_name as "Имя", last_name as "Фамилия", phone_number as "Номер телефона", date as "Дата запроса" FROM dbo.contacts WHERE checked IS NULL')
+	rows = cursor.fetchall()
+	column_names = [desc[0] for desc in cursor.description]
+	return rows, column_names
+
+def contact_status_update():
+	conn = psycopg2.connect(
+		dbname=db_config['dbname'],
+		user=db_config['user'],
+		password=db_config['password'],
+		host=db_config['host'],
+		port=db_config['port']
+	)
+	cursor = conn.cursor()
+	cursor.execute('UPDATE dbo.contacts SET checked = 1 WHERE checked IS NULL')
+	conn.commit()
+
+def format_contact_row(row, column_names):
+	"""Форматирует строку в читаемое сообщение"""
+	header = "Пользователь просит связаться с ним\n\n"
+	info = "\n".join([f"{col}: {val}" for col, val in zip(column_names, row)])
+	return header + info
+
+def send_new_contacts(chat_id):
+	try:
+		rows, columns = fetch_new_contact()
+		message = format_contact_row(rows, columns)
+		if not rows:
+			pass
+		else:
+			for row in rows:
+				message = format_contact_row(row, columns)
+				bot.send_message(CHAT_ID, message)
+				time.sleep(1)  # Задержка между сообщениями
+		contact_status_update()
+	except Exception as e:
+		bot.send_message(CHAT_ID, f"Ошибка при отправке записей из таблицы: {str(e)}")
+
+def run_scheduler():
+	"""Запускает проверку каждые 60 минут"""
+	chat_id = CHAT_ID
+	schedule.every(1).minutes.do(lambda: send_new_contacts(chat_id))
+
+	while True:
+		schedule.run_pending()
+		time.sleep(1)
+
+
 def format_table(column_names, rows):
 	# Форматируем таблицу
 	table = ''
@@ -109,11 +169,6 @@ def get_all_data(message):
 	table = format_table(column_names, rows)
 	send_table(message.chat.id, table)
 
-message_ids = []
-
-# @bot.message_handler(func=lambda message: True)
-# def handle_message(message):
-# 	message_ids.append(message.message_id)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -133,13 +188,6 @@ def start_message(message):
 	first_message_id = new_start_message.message_id
 
 
-def clear_chat(message):
-# Удаляем все сообщения, кроме первого
-	for msg in message_ids:
-		# Пропускаем первое сообщение
-		if msg != first_message_id:
-			bot.delete_message(chat_id=message.chat.id, message_id=msg.message_id)
-
 @bot.message_handler(content_types=['text'])
 def on_click(message):
 	if message.text.lower() == '🆕 вывести список новых пользователей':
@@ -156,5 +204,7 @@ def on_click(message):
 	else:
 		# clear_chat(message)
 		bot.send_message(message.chat.id, f'Выбери действие из меню')
-
+# Запускаем планировщик в отдельном потоке
+scheduler_thread = Thread(target=run_scheduler)
+scheduler_thread.start()
 bot.infinity_polling()
