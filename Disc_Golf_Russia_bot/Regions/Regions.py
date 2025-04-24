d0701093d4495e65839ebf63a7e5987bde89121a
Ivan Chapanov <ichapanov@navicons.com>
2025-04-25 01:09:53 +0500

    added parks to Region dict

--- Disc_Golf_Russia_bot/Regions/Regions.py
+++ Disc_Golf_Russia_bot/Regions/Regions.py
@@ -35,11 +35,292 @@ regions = {
                         'name': 'Тренировочный'
                     }
                 }
+            }
+        }
+    },
+    'санкт-петербург':{
+        'fullname': 'Санкт-Петербург',
+        'shortname': 'Спб',
+        'region_channel': 'https://t.me/discgolfspb',
+        'park': {
+            'Туутари': {
+                'address': 'Ленинградская область, Ломоносовский район, деревня Ретселя, 14',
+                'metrix_link': 'https://discgolfmetrix.com/course/38028',
+                'latitude': 59.694118,
+                'longitude': 30.175228,
+                'cources':{
+                    'Amateur 18 layout':{
+                        'name': ' Amateur 18 layout'
+                    },
+                    'Pro 18 layout': {
+                        'name': 'Pro 18 layout'
+                    }
+                }
+            },
+            'Дюны': {
+                'address': 'г.Санкт-Петербург, Сестрорецк, Заречная дорога 1, санаторий "Дюны"',
+                'metrix_link': 'https://discgolfmetrix.com/course/27811',
+                'latitude': 60.123044,
+                'longitude': 29.948834,
+                'cources':{
+                    'Dunes, 18 holes':{
+                        'name': 'Dunes, 18 holes'
+                    },
+                    'Dunes 9 holes': {
+                        'name': 'Dunes 9'
+                    }
+                }
+            },
+            'Загородный Клуб "ДАЧА"': {
+                'address': 'Ленинградская область, Приозерский район, до 69 км старого Приозерского шоссе, далее по указателям',
+                'metrix_link': 'https://discgolfmetrix.com/course/23673',
+                'latitude': 60.67233777,
+                'longitude': 30.16742706,
+                'cources':{
+                    'Country Club DACHA 9':{
+                        'name': 'Country Club DACHA 9'
 
+                    }
+                }
+            }
+        }
+    },
+    'нижний новгород':{
+        'fullname': 'Нижний Новгород',
+        'shortname': 'Нн',
+        'region_channel': 'https://t.me/discgolf_nn',
+        'park': {
+            'Парк Славы': {
+                'address': 'г.Нижний Новгород, Автозаводский район,парк Славы',
+                'metrix_link': 'https://discgolfmetrix.com/course/27296',
+                'latitude': 56.246276,
+                'longitude': 43.871095,
+                'cources':{
+                    'NN_Slava_Pro':{
+                        'name': ' NN_Slava_Pro'
+                    }
+                }
+            },
+            'Парк Приокский': {
+                'address': 'г.Нижний Новгород, проспект Гагарина, парк Приокский',
+                'metrix_link': 'https://discgolfmetrix.com/course/28276',
+                'latitude': 56.231122,
+                'longitude': 43.943699,
+                'cources':{
+                    'Лига':{
+                        'name': 'Лига'
+                    },
+                    'Основной': {
+                        'name': 'Основной'
+                    }
+                }
+            }
+        }
+    },
+    'псков':{
+        'fullname': 'Псков',
+        'shortname': 'Пск',
+        'region_channel': 'https://t.me/zvanskoe',
+        'park': {
+            'Званское': {
+                'address': 'Псковская область, Псковский район, Деревня 1-е Подборовье, 1/1',
+                'metrix_link': 'https://discgolfmetrix.com/course/27296',
+                'latitude': 57.891129,
+                'longitude': 28.635400,
+                'cources':{
+                    'RED champion 18':{
+                        'name': 'RED champion 18'
+                    },
+                    'GREEN (Am) - main_18':{
+                        'name': 'GREEN (Am) - main_18'
+                    }
+                }
+            }
+        }
+    },
+    'великие луки':{
+        'fullname': 'Великие Луки',
+        'shortname': 'ВЛ',
+        'region_channel': 'https://t.me/DiscGolf_VL',
+        'park': {
+            'Званское': {
+                'address': 'Псковская область, г.Великие Луки, парк Петровский',
+                'metrix_link': 'https://discgolfmetrix.com/course/27296',
+                'latitude': 56.343723,
+                'longitude': 30.508359,
+                'cources':{
+                    'Ligue_15(5+5+5)':{
+                        'name': 'Ligue_15(5+5+5)'
+                    }
+                }
+            }
+        }
+    },
+    'рыбинск':{
+        'fullname': 'Рыбинск',
+        'shortname': 'РБ',
+        'region_channel': 'https://t.me/DiscGolfRybinsk',
+        'park': {
+            'Eagle Park': {
+                'address': 'Ярославская обл., г. Рыбинск, д. Орловское',
+                'metrix_link': 'https://discgolfmetrix.com/course/39446',
+                'latitude': 57.956436,
+                'longitude': 38.856187,
+                'cources':{
+                    'PRO18':{
+                        'name': 'PRO18'
+                    },
+                    'AM9':{
+                        'name': 'AM9'
+                    }
+                }
+            }
+        }
+    },
+    'белгород':{
+        'fullname': 'Белгород',
+        'shortname': 'БЛГ',
+        'region_channel': 'https://t.me/discgolf31',
+        'park': {
+            'Belogorie диск-гольф парк': {
+                'address': 'г.Белгород, ул.Волчанская, в районе дома № 203',
+                'metrix_link': 'https://discgolfmetrix.com/course/24435',
+                'latitude': 50.540836,
+                'longitude': 36.665222,
+                'cources':{
+                    ' ':{
+                        'name': ' '
+                    },
+                    ' ':{
+                        'name': ' '
+                    }
+                }
+            }
+        }
+    },
+    'калининград':{
+        'fullname': 'Калининград',
+        'shortname': 'КЛГ',
+        'region_channel': 'https://t.me/discgolf_Kaliningrad',
+        'park': {
+            'Ростех Арена': {
+                'address': 'г.Калининград, Солнечный бульвар, 25',
+                'metrix_link': 'https://discgolfmetrix.com/?u=course&ID=29321',
+                'latitude': 54.698171,
+                'longitude': 20.536168,
+                'cources':{
+                    ' ':{
+                        'name': ' '
+                    },
+                    ' ':{
+                        'name': ' '
+                    }
+                }
+            }
+        }
+    },
+    'набережные челны':{
+        'fullname': 'Набережные Челны',
+        'shortname': 'НЧ',
+        'region_channel': 'https://t.me/discgolf_nch',
+        'park': {
+            'Шильна': {
+                'address': 'Республика Татарстан, Тукаевский район, деревня Малая Шильна, квартал Маленькая страна ',
+                'metrix_link': 'https://discgolfmetrix.com/course/35073',
+                'latitude': 55.815539,
+                'longitude': 52.541674,
+                'cources':{
+                    'ЧР 2024':{
+                        'name': 'ЧР 2024'
+                    },
+                    ' ':{
+                        'name': ' '
+                    }
+                }
+            },
+            'Прибрежный': {
+                'address': 'Республика Татарстан, г.Набережные Челны, проспект Хасана Туфана, 78',
+                'metrix_link': 'https://discgolfmetrix.com/course/35073',
+                'latitude': 55.748034,
+                'longitude': 52.372971,
+                'cources':{
+                    '18 корзин':{
+                        'name': '18 корзин'
+                    },
+                    '9 корзин':{
+                        'name': '9 корзин'
+                    }
+                }
+            }
+        }
+    },
+    'тольятти':{
+        'fullname': 'Тольятти',
+        'shortname': 'ТЛТ',
+        'region_channel': 'https://t.me/tfdf63',
+        'park': {
+            'H3': {
+                'address': 'Самарская область, г.Тольятти, Комсомольский район, парк культуры и отдыха',
+                'metrix_link': 'https://discgolfmetrix.com/course/35073',
+                'latitude': 53.480918,
+                'longitude': 49.466028,
+                'cources':{
+                    'Турнирная':{
+                        'name': 'Турнирная'
+                    }
+                }
+            },
+            'Жигули': {
+                'address': 'Самарская область, г.Тольятти, Автозаводский район, Южное шоссе, 165',
+                'metrix_link': 'https://discgolfmetrix.com/course/35073',
+                'latitude': 53.556812,
+                'longitude': 49.214913,
+                'cources':{
+                    'Жигули':{
+                        'name': 'Жигули'
+                    }
+                }
+            }
+        }
+    },
+    'москва':{
+        'fullname': 'Москва',
+        'shortname': 'МСК',
+        'region_channel': 'https://t.me/discgolfpro',
+        'park': {
+            'Сколково': {
+                'address': 'г. Москва, инновационный центр "Сколково"',
+                'metrix_link': 'https://discgolfmetrix.com/course/35073',
+                'latitude': 55.691767,
+                'longitude': 37.349539,
+                'cources':{
+                    'Турнирная схема TS21':{
+                        'name': 'Турнирная схема TS21'
+                    },
+                    'Для любителей TS14':{
+                        'name': 'Для любителей TS14'
+                    }
+
+                }
+            },
+            'Дворец Пионеров': {
+                'address': 'г. Москва,ул. Косыгина, д.17, корп. 1',
+                'metrix_link': 'https://discgolfmetrix.com/course/35073',
+                'latitude': 55.705646,
+                'longitude': 37.554387,
+                'cources':{
+                    'Турнирная схема 2025 TS18':{
+                        'name': 'Турнирная схема 2025 TS18'
+                    },
+                    'Для начинающих 2022 F9':{
+                        'name': 'Для начинающих 2022 F9'
+                    }
+                }
             }
         }
     }
 }
+
 
 
 class Region:
